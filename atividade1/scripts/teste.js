2593bb2cc0f5460b4a42a6bd2d924840c27abeb7
EltonGomesDevBr <eltongomes415@gmail.com>
2023-06-28 14:21:09 -0300

    atividad1

---
+++ atividade1/scripts/teste.js
@@ -0,0 +1,15 @@
+
+
+function solucao(altitude) {
+   
+      if  (altitude == 20 || altitude <= 49 ){
+               console.log("Troposfera")
+      }
+        
+    }
+               
+
+
+
+
+console.log(solucao(20))
